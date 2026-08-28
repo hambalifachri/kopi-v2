ef657baf3c53124bec7634de73302e7ebbbd1aa5
hambalifachri <igneelmyth@gmail.com>
2026-08-28 12:50:02 +0700

    Preserve apostrophes in outlet searches

--- tools/kopken-menu-sync/sync.mjs
+++ tools/kopken-menu-sync/sync.mjs
@@ -337,7 +337,7 @@ async function openOutlet(outletName, firstOutlet = false, preciseClick = false)
   const text = searchName
     .replace(/\s/g, "%s")
     .replace(/([()])/g, "\\$1")
-    .replace(/[^\w%@.\\()\-/]/g, "");
+    .replace(/[^\w%@.'\\()\-/]/g, "");
   runAdb(["shell", "svc", "power", "stayon", "true"]);
   runAdb(["shell", "input", "keyevent", "224"]);
   runAdb(["shell", "wm", "dismiss-keyguard"]);
